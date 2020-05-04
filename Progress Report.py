ec42f4ff2f62cab0b791be7eed3c9ffb03a2ffa7
Nishit2007 <64786303+Nishit2007@users.noreply.github.com>
2020-05-04 19:25:29 +0530

    Add files via upload

---
+++ Progress Report.py
@@ -0,0 +1,11 @@
+sname=input("Enter Students Name : ")
+maths=int(input("Enter Maths Marks : "))
+english=int(input("Enter English Marks : "))
+science=int(input("Enter Science Marks : "))
+
+if maths>=40   and english>=40   and   science>=40 :
+    print("Congratulation",sname, "you passes and promoted to next grade")
+else :
+    print("Sorry",sname, "you failed and not promotec to next grade")
+    
+          
